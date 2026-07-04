1e1a7c684efcb24b5ea9b13c3bef8f1d237da441
Vasilii Pochkaenko <pochkaenkov@yandex.ru>
2026-07-04 18:43:58 +0700

    feat(nvim): C++ keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -114,9 +114,13 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "c", "cpp" },
 	group = g,
 	callback = function()
-		-- Отступы
-		vim.opt_local.cindent = true
-		vim.opt_local.cinoptions = ">s,:0,l1,g0,(0,Ws"
+		-- -- Отступы
+		-- vim.opt_local.cindent = true
+		-- vim.opt_local.cinoptions = ">s,:0,l1,g0,(0,Ws"
+
+		vim.keymap.set('n', '<Leader><Leader>', ':lua vim.lsp.buf.', { desc = 'Open C++ tools' })
+		vim.keymap.set('n', '<C-d>', vim.lsp.buf.hover, { desc = 'Open a documentation for a current symbol in a hover window' })
+		vim.keymap.set('n', '<F2>', vim.lsp.buf.declaration, { desc = 'Go to declaration' })
 	end,
 })
 
