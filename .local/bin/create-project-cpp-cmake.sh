2c95fb86d6a6185e2245f0f6528a229038c3d8dc
Vasilii Pochkaenko <pochkaenkov@yandex.ru>
2026-07-20 14:23:07 +0700

    feat(C++,nvim): auto trust .nvim.lua on creation of C++ project

--- .local/bin/create-project-cpp-cmake.sh
+++ .local/bin/create-project-cpp-cmake.sh
@@ -3,3 +3,6 @@ PROJECT_NAME="${1:-new-project}"
 mkdir "$PROJECT_NAME"
 cp -r ~/.config/nvim/cpp_cmake_template "$PROJECT_NAME/$PROJECT_NAME"
 sed -i "s/cppExample/$PROJECT_NAME/g" "$PROJECT_NAME/$PROJECT_NAME/CMakeLists.txt"
+
+cd "$PROJECT_NAME/$PROJECT_NAME"
+nvim +trust +q --headless .nvim.lua
