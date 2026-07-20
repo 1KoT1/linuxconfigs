3e2808cb9515853628b1f4a9ac0ca028707c6a5c
Vasilii Pochkaenko <pochkaenkov@yandex.ru>
2026-07-20 19:21:51 +0700

    feat(C++,nvim): add a quick command for build

--- .config/nvim/cpp_cmake_template/.nvim.lua
+++ .config/nvim/cpp_cmake_template/.nvim.lua
@@ -1,3 +1,5 @@
 require("cmake-support")
 require("cpp-debug-support")
 require("cpp-tools").setup({})
+
+vim.api.nvim_create_user_command( 'Build', 'CMakeBuild', { desc = 'Save and build a project' })
